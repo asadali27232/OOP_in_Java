09b91e2720d415e5707f524fda23f785135b49e7
Asad Ali <asadali27232@gmail.com>
2022-02-20 13:58:05 +0500

    Chapter 2

--- Book Exercises/Chapter 2/Question2.16/src/Main.java
+++ Book Exercises/Chapter 2/Question2.16/src/Main.java
@@ -5,6 +5,30 @@ public class Main {
         Scanner input = new Scanner(System.in);
         System.out.print("Enter a Number: ");
         int x = input.nextInt();
-        
+        int sq = x*x;
+        if (x>100)
+        {
+            System.out.printf("Number %d is greater then 100.%n", x);
+        }
+        if (x<100)
+        {
+            System.out.printf("Number %d is less then 100.%n", x);
+        }
+        if (x==100)
+        {
+            System.out.printf("Number %d is Equal to 100.%n", x);
+        }
+        if (sq>100)
+        {
+            System.out.printf("The Square %d of Number %d is greater then 100.%n", sq, x);
+        }
+        if (sq<100)
+        {
+            System.out.printf("The Square %d of Number %d is less then 100.%n", sq, x);
+        }
+        if (sq==100)
+        {
+            System.out.printf("The Square %d of Number %d is equal to 100.%n", sq, x);
+        }
     }
 }

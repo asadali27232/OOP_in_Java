8dc15ea00b2a19057ebee6a485d8ad9ab8be7083
Asad Ali <77291718+asadali27232@users.noreply.github.com>
2022-02-22 12:55:34 +0500

    Task 4.1

---
+++ LAB_Tasks/LAB_3-4/LAB_Taksk_4.1/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter Radius of the circle: ");
+        double radius = input.nextDouble();
+
+        Circle c = new Circle();
+
+        System.out.println("Area of circle is: " + c.area(radius));
+        System.out.println("Circumference of circle is: " + c.circumference(radius));
+        c.getRadius();
+        c.setRadius(radius);
+        c.getRadius();
+    }
+}

10c36659a4402e3243e6c282d454279e158c0056
Asad Ali <asadali27232@gmail.com>
2022-02-22 18:09:46 +0500

    no comment

---
+++ LAB_Tasks/LAB_3-4/LAB_Task_4.1/Circle.java
@@ -0,0 +1,26 @@
+
+public class Circle {
+    private double radius;
+
+
+    public Circle(){
+        radius = 0;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double r){
+        radius = r;
+    }
+
+    public double area(double r){
+        radius = r;
+        return (3.14*radius*radius);
+    }
+    public double circumference(double r){
+        radius = r;
+        return (2*3.14*radius);
+    }
+}
